93678e8c29c79ca9ec7d76c98d16e4d8576350d1
kiruthika-netizen <kiruthika@krylox.ai>
2026-06-05 16:00:19 +0530

    delete pratice.py
    Removed FastAPI application and Student model.

--- pratice.py
+++ pratice.py
@@ -1,16 +1,1 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 
-app = FastAPI()
-
-class Student(BaseModel):
-    name: str
-    age: int
-    course: str
-
-@app.post("/student")
-async def create_student(student: Student):
-    return {
-        "message": "Student Created Successfully",
-        "data": student
-    }
